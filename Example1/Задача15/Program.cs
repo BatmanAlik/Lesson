a77ddaa87e9288b7318a10269c40cf32cb901c01
Alik <i@apogosov.ru>
2022-08-13 23:54:39 +0300

    Задачи 13 и 15

---
+++ Example1/Задача15/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine ("Введите день недели ");
+int Number = Convert.ToInt32 (Console.ReadLine ());
+string NumberText = Convert.ToString(Number);
+void CheckingTheday (int Number) {
+if (Number == 6 || Number == 7) 
+    { Console.WriteLine("этот день выходной? - да");
+
+
+    }    
+    else if (Number < 1 || Number > 7) 
+        { Console.WriteLine ("это вообще не день недели");
+
+        }
+    else Console.WriteLine("этот день выходной? - нет");
+}
+CheckingTheday (Number);
